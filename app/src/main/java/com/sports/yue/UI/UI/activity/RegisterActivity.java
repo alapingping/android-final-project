0ee736f8144a25c77c3ba84afba49f6e4842d9c2
lixingyuan1998 <38291879+lixingyuan1998@users.noreply.github.com>
2018-12-24 22:30:02 +0800

    Login UI beauty

--- app/src/main/java/com/sports/yue/UI/UI/activity/RegisterActivity.java
+++ app/src/main/java/com/sports/yue/UI/UI/activity/RegisterActivity.java
@@ -10,12 +10,16 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.sports.yue.R;
+import com.sports.yue.UI.UI.Adapter.OnSwipeTouchListener;
 import com.sports.yue.UI.UI.Database_operation.Db_operation;
 import com.sports.yue.UI.UI.api.BmobService;
 import com.sports.yue.UI.UI.api.Client;
@@ -38,6 +42,10 @@ import retrofit2.http.Body;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+    ImageView imageView;
+    TextView textView;
+    int count = 0;
     private JSONObject jsonObject = null;
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -48,7 +56,46 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_register);
+
+        imageView = findViewById(R.id.register_imageView);
+        textView = findViewById(R.id.register_textView);
+        imageView.setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()) {
+            public void onSwipeTop() {
+            }
+
+            public void onSwipeRight() {
+                if (count == 0) {
+                    imageView.setImageResource(R.drawable.good_night_img);
+                    textView.setText("Night");
+                    count = 1;
+                } else {
+                    imageView.setImageResource(R.drawable.good_morning_img);
+                    textView.setText("Morning");
+                    count = 0;
+                }
+            }
+
+            public void onSwipeLeft() {
+                if (count == 0) {
+                    imageView.setImageResource(R.drawable.good_night_img);
+                    textView.setText("Night");
+                    count = 1;
+                } else {
+                    imageView.setImageResource(R.drawable.good_morning_img);
+                    textView.setText("Morning");
+                    count = 0;
+                }
+            }
+
+            public void onSwipeBottom() {
+            }
+
+        });
+
 
         TextView back = findViewById(R.id.backToLoginText);
         back.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
