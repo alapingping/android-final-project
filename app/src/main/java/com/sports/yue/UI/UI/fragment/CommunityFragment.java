897c379ac9fd122d05100dc6f3702859d76f3f44
lixingyuan1998 <38291879+lixingyuan1998@users.noreply.github.com>
2018-12-25 23:11:58 +0800

    点赞

--- app/src/main/java/com/sports/yue/UI/UI/fragment/CommunityFragment.java
+++ app/src/main/java/com/sports/yue/UI/UI/fragment/CommunityFragment.java
@@ -1,7 +1,9 @@
 package com.sports.yue.UI.UI.fragment;
 
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -21,6 +23,7 @@ import android.widget.Toast;
 
 //import com.sports.yue.UI.UI.Adapter.VideoAdapter;
 
+import com.jaren.lib.view.LikeView;
 import com.sports.yue.R;
 import com.sports.yue.UI.UI.Adapter.VideoAdapter;
 import com.sports.yue.UI.UI.Database_operation.Db_operation;
@@ -32,6 +35,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
@@ -60,6 +64,73 @@ public class CommunityFragment extends Fragment {
     private AbsListView.OnScrollListener onScrollListener;
     JCVideoPlayerStandard jcVideoPlayerStandard;
     private WaveSwipeRefreshLayout mWaveSwipeRefreshLayout;
+    private LikeView mLikeView;
+    private LikeView mLikeViewSet;
+
+
+    interface Callback {
+
+        void onSuccess();
+
+        void onFail();
+    }
+    private void useSet() {
+        requestPraise(new Callback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getContext(),"toggle Success ",Toast.LENGTH_SHORT).show();
+                if (mLikeViewSet.isChecked()) {
+                    mLikeViewSet.setChecked(false);
+//                  mLikeViewSet.setCheckedWithoutAnimator(false);
+                } else {
+                    mLikeViewSet.setChecked(true);
+                }
+            }
+            @Override
+            public void onFail() {
+                Toast.makeText(getContext(),"toggle Fail ",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void useToggle() {
+        mLikeView.toggle();
+        requestPraise(new Callback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getContext(),"toggle Success ",Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFail() {
+                mLikeView.toggleWithoutAnimator();
+                Toast.makeText(getContext(),"toggle Fail ",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void requestPraise(final Callback callback) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(400);
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Random random = new Random();
+                            if (random.nextInt() % 2 == 0) {
+                                callback.onSuccess();
+                            } else {
+                                callback.onFail();
+                            }
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
 
     private class Task extends AsyncTask<Void, Void, String[]> {
         @Override
@@ -70,10 +141,12 @@ public class CommunityFragment extends Fragment {
         @Override
         protected void onPostExecute(String[] result) {
             // Call setRefreshing(false) when the list has been refreshed.
+           // mWaveSwipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.lightgreen),getResources().getColor(R.color.colorPrimary),getResources().getColor(R.color.red),getResources().getColor(R.color.greenyellow));
             mWaveSwipeRefreshLayout.setRefreshing(false);
             super.onPostExecute(result);
         }
     }
+
 
 
     @Override
@@ -83,18 +156,35 @@ public class CommunityFragment extends Fragment {
 
         // Inflate the layout for this fragment
         mWaveSwipeRefreshLayout = (WaveSwipeRefreshLayout)view.findViewById(R.id.main_swipe);
+//        mWaveSwipeRefreshLayout.setColorSchemeResources(Color.parseColor("#FF4081"),Color.parseColor("#303F9F"));
+
         mWaveSwipeRefreshLayout.setOnRefreshListener(new WaveSwipeRefreshLayout.OnRefreshListener() {
             @Override public void onRefresh() {
                 // Do work to refresh the list here.
+
                 Db_operation.getDb_op().searchCommunity();
+
                 Toast.makeText(getActivity(), "更新完成",Toast.LENGTH_LONG).show();
                 new Task().execute();
             }
         });
 
 
+        mLikeView = view.findViewById(R.id.lv);
+        mLikeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                useToggle();
+            }
+        });
 
-
+        mLikeViewSet = view.findViewById(R.id.lv_set);
+        mLikeViewSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                useSet();
+            }
+        });
 
         Button findButton = view.findViewById(R.id.add_community);
         findButton.setOnClickListener(new View.OnClickListener() {
@@ -120,6 +210,9 @@ public class CommunityFragment extends Fragment {
 
         return view;
     }
+
+
+
 
 
     @Override
